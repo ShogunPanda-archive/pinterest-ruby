5308511b2df671b7a611bfaf29640a992a3028a0
Shogun <shogun@cowtech.it>
2018-06-18 10:05:15 +0200

    chore: Version 1.0.1.

--- lib/pinterest/version.rb
+++ lib/pinterest/version.rb
@@ -15,7 +15,7 @@ module Pinterest
     MINOR = 0
 
     # The patch version.
-    PATCH = 0
+    PATCH = 1
 
     # The current version of pinterest-ruby.
     STRING = [MAJOR, MINOR, PATCH].compact.join(".")
